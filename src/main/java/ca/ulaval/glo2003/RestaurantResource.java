f9bf8b182e6e3748f0675595a8a2bec43ee5070c
Edwin Lemelin <edwin.lemelin.1@ulaval.ca>
2024-02-05 15:51:51 -0500

    fix: modify getRestaurant to work properly

--- src/main/java/ca/ulaval/glo2003/RestaurantResource.java
+++ src/main/java/ca/ulaval/glo2003/RestaurantResource.java
@@ -20,30 +20,16 @@ public class RestaurantResource {
     Map<String, Restaurant> restaurantIdToRestaurant = new HashMap<>();
     Map<String, String> restaurantIdToOwnerId = new HashMap<>();
 
-    @Path("restaurants/{id}")
     @GET
+    @Path("restaurants/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getRestaurant(@PathParam("id") String restaurantId, @HeaderParam("Owner") String ownerId)
-    {
-        if (ownerId == null) throw new NullPointerException("Owner is missing");
-        if (restaurantIdToRestaurant.get(restaurantId).getCapacity() == 0) {
-            throw new NullPointerException("Capacity is missing");
-        }
-        /*if (restaurantIdToRestaurant.get(restaurantId).getOpeningTime() == null){
-            throw new NullPointerException("Opening time is missing");
-        }
-        if (restaurantIdToRestaurant.get(restaurantId).getClosingTime() == null){
-            throw new NullPointerException("Closing time is missing");
-        }*/
+    public Response getRestaurant(@PathParam("id") String restaurantId, @HeaderParam("Owner") String ownerId) {
+        if (ownerId == null) throw new NullPointerException("Owner id must be provided");
         if (!restaurantIdToRestaurant.containsKey(restaurantId)) {
-            return Response.status(Response.Status.NOT_FOUND)
-                    .entity("The restaurant does not exist.")
-                    .build();
+            return Response.status(Response.Status.NOT_FOUND).build();
         }
         if (!restaurantIdToOwnerId.get(restaurantId).equals(ownerId)) {
-            return Response.status(Response.Status.NOT_FOUND)
-                    .entity("The restaurant does not belong to the owner")
-                    .build();
+            return Response.status(Response.Status.NOT_FOUND).build();
         }
         return Response.status(Response.Status.OK)
                 .entity(restaurantIdToRestaurant.get(restaurantId))
@@ -54,7 +40,7 @@ public class RestaurantResource {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     public Response createRestaurant(@HeaderParam("Owner") String owner,RestaurantRequest restaurant) {
-        if (owner == null) throw new NullPointerException("Owner id should be provided");
+        if (owner == null) throw new NullPointerException("Owner id must be provided");
         Restaurant entity = new Restaurant(
                 restaurant.name,
                 restaurant.capacity,
@@ -79,8 +65,8 @@ public class RestaurantResource {
     @GET
     @Path("restaurants")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getRestaurant(@HeaderParam("Owner") String ownerId) {
-        if (ownerId == null) throw new NullPointerException("Owner id should be provided");
+    public Response listRestaurants(@HeaderParam("Owner") String ownerId) {
+        if (ownerId == null) throw new NullPointerException("Owner id must be provided");
         if (!ownerIdToRestaurantsId.containsKey(ownerId)) {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
