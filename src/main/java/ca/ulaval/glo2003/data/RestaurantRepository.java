3973dd4e06a0fb8b82eca8360d40fbb306d6753f
Alex Girard <alex.girard.2@ulaval.ca>
2024-02-23 16:01:48 -0500

    feat: add searchRestaurantRequest and searchResource

--- src/main/java/ca/ulaval/glo2003/data/RestaurantRepository.java
+++ src/main/java/ca/ulaval/glo2003/data/RestaurantRepository.java
@@ -1,15 +1,43 @@
 package ca.ulaval.glo2003.data;
 
+import ca.ulaval.glo2003.api.requests.SearchRestaurantsRequest;
 import ca.ulaval.glo2003.domain.entities.Restaurant;
+import ca.ulaval.glo2003.domain.entities.Search;
 
+import java.time.LocalTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class RestaurantRepository {
 
-    Map<String, List<String>> ownerIdToRestaurantsId = new HashMap<>();
-    Map<String, Restaurant> restaurantIdToRestaurant = new HashMap<>();
-    Map<String, String> restaurantIdToOwnerId = new HashMap<>();
-    
+    Map<String, Restaurant> restaurantIdToRestaurant;
+
+    public RestaurantRepository() {
+        restaurantIdToRestaurant = new HashMap<>();
+    }
+
+    public List<Restaurant> searchRestaurants(Search search) {
+        return restaurantIdToRestaurant.values().stream()
+                .filter(restaurant -> matchesRestaurantName(restaurant, search.getName()))
+                .filter(restaurant -> matchesRestaurantOpenHour(restaurant, search.getSearchOpened().getFrom()))
+                .filter(restaurant -> matchesRestaurantCloseHour(restaurant, search.getSearchOpened().getTo()))
+                .collect(Collectors.toList());
+    }
+    private boolean matchesRestaurantName(Restaurant restaurant, String name) {
+        if (name.isEmpty()){
+            return true;
+        }
+        return restaurant.getName().toLowerCase().replaceAll("\\s", "")
+                .contains(name.toLowerCase().replaceAll("\\s", ""));
+    }
+
+    private boolean matchesRestaurantOpenHour(Restaurant restaurant, LocalTime from) {
+        return restaurant.getHours().getOpen().isAfter(from);
+    }
+
+    private boolean matchesRestaurantCloseHour(Restaurant restaurant, LocalTime to) {
+        return restaurant.getHours().getClose().isBefore(to);
+    }
 }
---
+++ src/main/java/ca/ulaval/glo2003/domain/SearchService.java
@@ -0,0 +1,35 @@
+package ca.ulaval.glo2003.domain;
+
+import ca.ulaval.glo2003.data.RestaurantRepository;
+import ca.ulaval.glo2003.domain.dto.RestaurantDto;
+import ca.ulaval.glo2003.domain.dto.SearchDto;
+import ca.ulaval.glo2003.domain.entities.Restaurant;
+import ca.ulaval.glo2003.domain.entities.Search;
+import ca.ulaval.glo2003.domain.mappers.RestaurantMapper;
+import ca.ulaval.glo2003.domain.mappers.SearchMapper;
+
+import java.sql.PreparedStatement;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SearchService {
+    private final SearchMapper searchMapper;
+    private final RestaurantMapper restaurantMapper;
+    private final RestaurantRepository restaurantRepository;
+
+    public SearchService() {
+        searchMapper = new SearchMapper();
+        restaurantMapper = new RestaurantMapper();
+        restaurantRepository = new RestaurantRepository();
+    }
+
+    public List<RestaurantDto> searchRestaurants(SearchDto searchDto) {
+        Search search = searchMapper.fromDto(searchDto);
+
+        List<Restaurant> restaurants = restaurantRepository.searchRestaurants(search);
+
+        return restaurants.stream()
+                .map(restaurantMapper::toDto)
+                .collect(Collectors.toList());
+    }
+}
