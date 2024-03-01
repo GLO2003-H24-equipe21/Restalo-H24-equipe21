6e489a88b5ddfb66cda3ac1b997102372e50016c
Edwin Lemelin <edwin.lemelin.1@ulaval.ca>
2024-02-29 22:03:49 -0500

    fix: repair and refactor RestaurantResource

--- src/main/java/ca/ulaval/glo2003/domain/RestaurantService.java
+++ src/main/java/ca/ulaval/glo2003/domain/RestaurantService.java
@@ -1,33 +1,53 @@
 package ca.ulaval.glo2003.domain;
 
-import ca.ulaval.glo2003.data.ReservationRepository;
 import ca.ulaval.glo2003.data.RestaurantRepository;
-import ca.ulaval.glo2003.domain.dto.CustomerDto;
 import ca.ulaval.glo2003.domain.dto.RestaurantDto;
 import ca.ulaval.glo2003.domain.dto.RestaurantHoursDto;
 import ca.ulaval.glo2003.domain.dto.RestaurantReservationsDto;
-import ca.ulaval.glo2003.domain.entities.Reservation;
 import ca.ulaval.glo2003.domain.entities.Restaurant;
+import ca.ulaval.glo2003.domain.entities.RestaurantHours;
+import ca.ulaval.glo2003.domain.entities.RestaurantReservations;
+import ca.ulaval.glo2003.domain.factories.RestaurantFactory;
+import ca.ulaval.glo2003.domain.factories.RestaurantHoursFactory;
+import ca.ulaval.glo2003.domain.factories.RestaurantReservationsFactory;
 import ca.ulaval.glo2003.domain.mappers.RestaurantMapper;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class RestaurantService {
     private final RestaurantRepository restaurantRepository;
+    private final RestaurantHoursFactory restaurantHoursFactory;
+    private final RestaurantReservationsFactory restaurantReservationsFactory;
+    private final RestaurantFactory restaurantFactory;
     private final RestaurantMapper restaurantMapper;
-    private final ReservationRepository reservationRepository;
 
-    public RestaurantService() {
-        restaurantRepository = new RestaurantRepository();
+    public RestaurantService(
+            RestaurantRepository restaurantRepository,
+            RestaurantFactory restaurantFactory,
+            RestaurantHoursFactory restaurantHoursFactory,
+            RestaurantReservationsFactory restaurantReservationsFactory) {
+        this.restaurantRepository = restaurantRepository;
+        this.restaurantFactory = restaurantFactory;
+        this.restaurantHoursFactory = restaurantHoursFactory;
+        this.restaurantReservationsFactory = restaurantReservationsFactory;
         restaurantMapper = new RestaurantMapper();
-        reservationRepository = new ReservationRepository();
     }
 
-    public String createRestaurant(String ownerId, String name, Integer capacity, RestaurantHoursDto restaurantHoursDto, RestaurantReservationsDto restaurantReservationsDto) {
-        Restaurant restaurant = RestaurantFactory.create(
-                ownerId, name, capacity, restaurantHoursDto, restaurantReservationsDto
-        );
+    public String createRestaurant(
+            String ownerId,
+            String name,
+            Integer capacity,
+            RestaurantHoursDto hoursDto,
+            RestaurantReservationsDto reservationsDto) {
+        RestaurantHours hours = restaurantHoursFactory.create(hoursDto.open, hoursDto.close);
+        RestaurantReservations reservations =
+                restaurantReservationsFactory.create(
+                        Objects.requireNonNullElse(reservationsDto, new RestaurantReservationsDto())
+                                .duration);
+        Restaurant restaurant =
+                restaurantFactory.create(ownerId, name, capacity, hours, reservations);
 
         restaurantRepository.add(restaurant);
 
@@ -36,35 +56,14 @@ public class RestaurantService {
 
     public RestaurantDto getRestaurant(String restaurantId, String ownerId) {
         Restaurant restaurant = restaurantRepository.get(restaurantId);
-
         if (!restaurant.getOwnerId().equals(ownerId)) {
             throw new IllegalArgumentException("EXCEPTION A CHANGER");
         }
-
-        return restaurantMapper.toDto(restaurant);
-    }
-
-    public RestaurantDto getRestaurant(String restaurantId) {
-        Restaurant restaurant = restaurantRepository.get(restaurantId);
-
         return restaurantMapper.toDto(restaurant);
     }
 
     public List<RestaurantDto> listRestaurants(String ownerId) {
-        List<Restaurant> restaurants = restaurantRepository.findByOwnerId(ownerId);
-
-        return restaurants.stream()
-                .map(restaurantMapper::toDto)
-                .collect(Collectors.toList());
-    }
-
-    public String createReservation(String date, String startTime, Integer groupSize, CustomerDto customerDto, RestaurantDto restaurantDto) {
-        Reservation reservation = ReservationFactory.create(
-                date, startTime, groupSize, customerDto, restaurantDto
-        );
-
-        reservationRepository.add(reservation);
-
-        return reservation.getNumber();
+        List<Restaurant> restaurants = restaurantRepository.getByOwnerId(ownerId);
+        return restaurants.stream().map(restaurantMapper::toDto).collect(Collectors.toList());
     }
 }
