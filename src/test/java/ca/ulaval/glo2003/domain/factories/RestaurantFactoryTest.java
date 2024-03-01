20e0e10f63a13579e07ba2b1360f0246ef1f2de9
Kim St-Pierre <kistp2@ulaval.ca>
2024-03-01 14:51:38 -0500

    feat: add tests for emptyName and capacity below one

--- src/test/java/ca/ulaval/glo2003/domain/factories/RestaurantFactoryTest.java
+++ src/test/java/ca/ulaval/glo2003/domain/factories/RestaurantFactoryTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import static org.mockito.Mockito.when;
 
@@ -52,6 +53,29 @@ class RestaurantFactoryTest {
         Assertions.assertThat(restaurant.getCapacity()).isEqualTo(CAPACITY);
         Assertions.assertThat(restaurant.getHours()).isEqualTo(HOURS);
         Assertions.assertThat(restaurant.getReservations()).isEqualTo(RESERVATIONS);
+    }
+
+    @Test
+    void EmptyName_createRestaurantShouldThrow() {
+        String emptyName = "";
+        assertThrows(IllegalArgumentException.class, () -> restaurantFactory.create(
+                OWNER_ID,
+                emptyName,
+                CAPACITY,
+                HOURS,
+                RESERVATIONS
+        ));
+    }
+    @Test
+    void CapacityBelowOne_createRestaurantShouldThrow() {
+        int belowOneCapacity = -2;
+        assertThrows(IllegalArgumentException.class, () -> restaurantFactory.create(
+                OWNER_ID,
+                RESTAURANT_NAME,
+                belowOneCapacity,
+                HOURS,
+                RESERVATIONS
+        ));
     }
 
 
