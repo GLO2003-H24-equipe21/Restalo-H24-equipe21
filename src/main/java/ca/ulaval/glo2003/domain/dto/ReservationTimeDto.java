a26d4673fa29627f66d79440aecaf6d850bd3d54
Alex Girard <alex.girard.2@ulaval.ca>
2024-02-29 20:17:38 -0500

    Add jakarta bean validations and request classes

--- src/main/java/ca/ulaval/glo2003/domain/dto/ReservationTimeDto.java
+++ src/main/java/ca/ulaval/glo2003/domain/dto/ReservationTimeDto.java
@@ -1,21 +1,6 @@
 package ca.ulaval.glo2003.domain.dto;
 
-import java.util.Objects;
-
 public class ReservationTimeDto {
     public String start;
     public String end;
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) return true;
-        if (obj == null || obj.getClass() != this.getClass()) return false;
-        var that = (ReservationTimeDto) obj;
-        return Objects.equals(this.start, that.start) && Objects.equals(this.end, that.end);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(start, end);
-    }
 }
--- src/main/java/ca/ulaval/glo2003/domain/dto/RestaurantDto.java
+++ src/main/java/ca/ulaval/glo2003/domain/dto/RestaurantDto.java
@@ -1,28 +1,10 @@
 package ca.ulaval.glo2003.domain.dto;
 
-import java.util.Objects;
-
 public class RestaurantDto {
+    public String ownerId;
     public String id;
     public String name;
     public Integer capacity;
     public RestaurantHoursDto hours;
     public RestaurantReservationsDto reservations;
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) return true;
-        if (obj == null || obj.getClass() != this.getClass()) return false;
-        var that = (RestaurantDto) obj;
-        return Objects.equals(this.id, that.id)
-                && Objects.equals(this.name, that.name)
-                && Objects.equals(this.capacity, that.capacity)
-                && Objects.equals(this.hours, that.hours)
-                && Objects.equals(this.reservations, that.reservations);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, capacity, hours, reservations);
-    }
 }
