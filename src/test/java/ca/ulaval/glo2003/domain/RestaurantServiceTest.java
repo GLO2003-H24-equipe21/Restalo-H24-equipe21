115ab06f85aafa54d290ae0cfd28cbfeaf0daf1f
Kim St-Pierre <kistp2@ulaval.ca>
2024-03-02 01:03:57 -0500

    feat: make givenInvalidId_thenThrowNullPointerException method

--- src/test/java/ca/ulaval/glo2003/domain/RestaurantServiceTest.java
+++ src/test/java/ca/ulaval/glo2003/domain/RestaurantServiceTest.java
@@ -10,6 +10,7 @@ import ca.ulaval.glo2003.domain.entities.RestaurantReservations;
 import ca.ulaval.glo2003.domain.factories.RestaurantFactory;
 import ca.ulaval.glo2003.domain.factories.RestaurantHoursFactory;
 import ca.ulaval.glo2003.domain.factories.RestaurantReservationsFactory;
+import ca.ulaval.glo2003.domain.mappers.RestaurantMapper;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -32,6 +33,7 @@ class RestaurantServiceTest {
     private static final String OPEN = "10:24:32";
     private static final String CLOSE = "22:24:32";
     private static final int DURATION = 120;
+    private static String restaurantId;
 
     RestaurantService restaurantService;
     @Mock
@@ -51,6 +53,7 @@ class RestaurantServiceTest {
     RestaurantDto restaurantDto;
     RestaurantHoursDto restaurantHoursDto;
     RestaurantReservationsDto restaurantReservationsDto;
+    RestaurantMapper restaurantMapper = new RestaurantMapper();
 
     @BeforeEach
     void setUp() {
@@ -60,6 +63,8 @@ class RestaurantServiceTest {
         restaurantReservationsDto = RestaurantTestUtils.createRestaurantReservationsDTO(DURATION);
 
         restaurant = new Restaurant(OWNER_ID, RESTAURANT_NAME, CAPACITY, restaurantHours, restaurantReservations);
+        restaurantId = restaurant.getId();
+
         restaurantDto = new RestaurantDto();
         restaurantDto.ownerId = OWNER_ID;
         restaurantDto.name = RESTAURANT_NAME;
@@ -95,7 +100,7 @@ class RestaurantServiceTest {
                 restaurantHoursDto,
                 restaurantReservationsDto
         );
-        assertEquals(restaurant.getId(), restaurantServiceId);
+        assertEquals(restaurantId, restaurantServiceId);
     }
     @Test
     void whenCreateRestaurantWithValidValues_thenRestaurantIsCreatedAndSaved() {
@@ -114,7 +119,24 @@ class RestaurantServiceTest {
                 restaurantReservationsDto
         );
         verify(restaurantRepository).add(restaurant);
+    }
 
+    @Test
+    void givenExistingId_thenFindsRestaurant() {
+        when(restaurantRepository.get(restaurantId)).thenReturn(restaurant);
+        RestaurantDto gottenRestaurant = restaurantService.getRestaurant(restaurantId, OWNER_ID);
+
+        Assertions.assertThat(gottenRestaurant.id).isEqualTo(restaurantMapper.toDto(restaurant).id);
+        Assertions.assertThat(gottenRestaurant.name).isEqualTo(restaurantMapper.toDto(restaurant).name);
+        Assertions.assertThat(gottenRestaurant.capacity).isEqualTo(restaurantMapper.toDto(restaurant).capacity);
+        Assertions.assertThat(gottenRestaurant.hours).isEqualTo(restaurantMapper.toDto(restaurant).hours);
+        Assertions.assertThat(gottenRestaurant.reservations).isEqualTo(restaurantMapper.toDto(restaurant).reservations);
+    }
+    @Test
+    void givenInvalidId_thenThrowNullPointerException() {
+        when(restaurantRepository.get("invalid_number")).thenReturn(null);
+        assertThrows(NullPointerException.class,
+                () -> restaurantService.getRestaurant("invalid_number", OWNER_ID));
     }
 
 
