8eb7b4b9b6e3e5cdbfa0fe9853d4bdd38c4d721b
Kim1850 <kim.st-pierre.2@ulaval.ca>
2024-03-28 16:23:27 -0400

    feat: create ReservationSearchResponse record

---
+++ src/main/java/ca/ulaval/glo2003/api/responses/ReservationSearchResponse.java
@@ -0,0 +1,11 @@
+package ca.ulaval.glo2003.api.responses;
+
+import ca.ulaval.glo2003.api.pojos.CustomerPojo;
+import ca.ulaval.glo2003.api.pojos.ReservationTimePojo;
+
+public record ReservationSearchResponse(
+       String number,
+       String date,
+       ReservationTimePojo time,
+       Integer groupSize,
+       CustomerPojo customer) {}
