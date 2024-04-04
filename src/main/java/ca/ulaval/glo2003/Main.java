13863183ed94e1867f37111c0fe27997506b594c
charleslav <charles01_lv-2000@hotmail.com>
2024-04-04 17:05:28 -0400

    <fix> Docker Workflow: try to fix the docker workflow

--- src/main/java/ca/ulaval/glo2003/Main.java
+++ src/main/java/ca/ulaval/glo2003/Main.java
@@ -3,14 +3,20 @@ package ca.ulaval.glo2003;
 import ca.ulaval.glo2003.api.HealthResource;
 import ca.ulaval.glo2003.api.exceptions.*;
 import java.net.URI;
+import java.util.Objects;
+
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
 public class Main {
-    public static final String BASE_URI = "http://0.0.0.0:8080/";
+    public static String BASE_URI = "http://0.0.0.0:8080/";
 
     public static HttpServer startServer() {
+        if (!Objects.isNull(System.getenv("PORT")))
+            BASE_URI = BASE_URI.replace("8080", System.getenv("PORT"));
+
+
         ApplicationContext applicationContext = new ApplicationContext();
 
         final ResourceConfig resourceConfig =
@@ -24,6 +30,7 @@ public class Main {
                         .register(new IllegalArgumentExceptionMapper())
                         .register(new RuntimeExceptionMapper())
                         .register(new NotFoundExceptionMapper());
+
 
         return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), resourceConfig);
     }
