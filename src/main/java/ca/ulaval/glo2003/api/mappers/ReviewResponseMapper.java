ea9d1b66112fd8e842a772068e543aa5fc1e3321
Edwin Lemelin <edwin.lemelin.1@ulaval.ca>
2024-04-29 13:54:30 -0400

    feat: add review entities and methods to api layer

---
+++ src/main/java/ca/ulaval/glo2003/api/mappers/ReviewResponseMapper.java
@@ -0,0 +1,18 @@
+package ca.ulaval.glo2003.api.mappers;
+
+import ca.ulaval.glo2003.api.responses.ReviewResponse;
+import ca.ulaval.glo2003.domain.entities.Review;
+
+import java.time.format.DateTimeFormatter;
+
+public class ReviewResponseMapper {
+    private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ISO_LOCAL_DATE;
+
+    public ReviewResponse from(Review review) {
+        return new ReviewResponse(
+                review.getRating(),
+                review.getComment(),
+                review.getDate().format(dateTimeFormatter)
+        );
+    }
+}
---
+++ src/main/java/ca/ulaval/glo2003/api/responses/ReviewResponse.java
@@ -0,0 +1,4 @@
+package ca.ulaval.glo2003.api.responses;
+
+public record ReviewResponse(Integer rating, String comment, String date) {
+}
