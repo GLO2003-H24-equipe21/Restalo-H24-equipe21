f350820e573e975e9c4f885431fcbfb312c43ce3
Edwin Lemelin <edwin.lemelin.1@ulaval.ca>
2024-04-29 13:55:45 -0400

    feat: add review entities and methods to domain layer

---
+++ src/main/java/ca/ulaval/glo2003/domain/ReviewRepository.java
@@ -0,0 +1,13 @@
+package ca.ulaval.glo2003.domain;
+
+import ca.ulaval.glo2003.domain.entities.Review;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface ReviewRepository {
+
+    void add(Review review);
+
+    List<Review> searchReviews(String restaurantId, List<Integer> ratings, LocalDate from, LocalDate to);
+}
---
+++ src/main/java/ca/ulaval/glo2003/domain/factories/ReviewFactory.java
@@ -0,0 +1,27 @@
+package ca.ulaval.glo2003.domain.factories;
+
+import ca.ulaval.glo2003.domain.entities.Review;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public class ReviewFactory {
+    public Review create(String restaurantId, Integer rating, String comment) {
+        verifyRatingBetween0And5(rating);
+        verifyCommentNotEmpty(comment);
+
+        return new Review(UUID.randomUUID().toString(), restaurantId, rating, comment, LocalDate.now());
+    }
+
+    private void verifyRatingBetween0And5(Integer rating) {
+        if (rating < 0 || rating > 5) {
+            throw new IllegalArgumentException("Review rating must be between 0 and 5");
+        }
+    }
+
+    private void verifyCommentNotEmpty(String comment) {
+        if (comment.isEmpty()) {
+            throw new IllegalArgumentException("Review comment must not be empty");
+        }
+    }
+}
