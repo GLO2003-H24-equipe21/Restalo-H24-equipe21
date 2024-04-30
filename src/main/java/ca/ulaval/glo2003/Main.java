cf931ec719ce22da5abbbcc7388fd28915b0000e
Edwin Lemelin <edwin.lemelin.1@ulaval.ca>
2024-04-30 15:35:00 -0400

    fix: modify searchAvailabilities method

--- src/main/java/ca/ulaval/glo2003/Main.java
+++ src/main/java/ca/ulaval/glo2003/Main.java
@@ -2,21 +2,18 @@ package ca.ulaval.glo2003;
 
 import ca.ulaval.glo2003.api.HealthResource;
 import ca.ulaval.glo2003.api.exceptions.*;
+import io.sentry.Sentry;
 import java.net.URI;
-import java.util.EmptyStackException;
 import java.util.Objects;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
-import io.sentry.Sentry;
-
 
 public class Main {
     public static String BASE_URI = "http://0.0.0.0:8080/";
 
     public static HttpServer startServer() {
         String port = System.getenv("PORT");
-        String dns;
         if (!Objects.isNull(port)) {
             BASE_URI = BASE_URI.replace("8080/", port + "/");
         }
@@ -36,17 +33,18 @@ public class Main {
                         .register(new IllegalArgumentExceptionMapper())
                         .register(new NotFoundExceptionMapper());
 
-        dns = System.getenv("SENTRY_DSN");
-
-        if (!Objects.isNull(dns)) {
-            Sentry.init(options -> {
-                options.setDsn(System.getenv("SENTRY_DSN"));
-                options.setEnableMetrics(true);
-                options.setTracesSampleRate(1.0);
-                options.setDebug(true);
-            });
+        String sentryDsn = System.getenv("SENTRY_DSN");
+        if (!Objects.isNull(sentryDsn)) {
+            Sentry.init(
+                    options -> {
+                        options.setDsn(System.getenv("SENTRY_DSN"));
+                        options.setEnableMetrics(true);
+                        options.setTracesSampleRate(1.0);
+                        options.setDebug(true);
+                    });
             Sentry.metrics().increment("start", 1);
         }
+
         return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), resourceConfig);
     }
 
